97f08f0c7bc5606537c733c6453a14ad1445c051
fxddev <79515689+fxddev@users.noreply.github.com>
2022-09-07 10:07:14 +0700

    Initial commit

---
+++ app/components/Home.svelte
@@ -0,0 +1,18 @@
+<page>
+    <actionBar title="Home" />
+    <gridLayout>
+        <label class="info" text={message} />
+    </gridLayout>
+</page>
+
+<script>
+    let message = "Hellow"
+</script>
+
+<style>
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
